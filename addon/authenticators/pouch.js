f98c0f9543c7c8051296295b906a1fef13cbd4ec
Joren l'Ami <joren@distinct.nl>
2016-08-26 13:48:17 +0200

    loginOpts for chrome fix logout emit getSession instead of getUser for restore

--- addon/authenticators/pouch.js
+++ addon/authenticators/pouch.js
@@ -5,13 +5,24 @@ const { getOwner } = Ember;
 export default Base.extend({
   store: Ember.inject.service(),
   
+  loginOpts: {},
+  
   init() {
     this._super(...arguments);
 
     let config = getOwner(this).resolveRegistration('config:environment');
 
     //let the user override the default adapter
-    let pouchAdapterName = config.emberPouch.authAdapter || 'user';
+    let pouchAdapterName = config.authAdapter || 'user';
+    if (config.pouchDb && config.pouchDb._fixChromeLoginBug) {//not needed for cloudant
+    	this.loginOpts = {
+			ajax: {
+				headers: {
+				  'X-Hello': 'World'
+				}
+			}
+		};
+    }
 
     let pouchAdapter = this.get('store').adapterFor(pouchAdapterName);
 
@@ -22,27 +33,30 @@ export default Base.extend({
 
   restore(data) {
   	let self = this;
-  	return this.db.getUser(data.name).then(function() {
-  		self.db.emit('loggedin');
-  		return data;
+  	return this.db.getSession().then(function(resp) {
+  		let result = null;
+  		if (!Ember.isEmpty(data.name) && resp.userCtx.name === data.name) {
+  			result = data;
+  			self.db.emit('loggedin');
+  		}
+  		else {
+  			result = Ember.RSVP.reject("Not logged in or incorrect user in cookie");
+  		}
+  		
+  		return result;
   	});
   },
 
   authenticate(username, password) {
   	let self = this;
-    return this.db.login(username, password, {
-		ajax: {
-			headers: {
-			  'X-Hello': 'World'
-			}
-		}
-	}).then(function(data) {
+    return this.db.login(username, password, this.loginOpts).then(function(data) {
 		self.db.emit('loggedin');
 		return data;
 	});
   },
 
   invalidate() {
+  	this.db.emit('loggedout');
     return this.db.logout();
   }
 });
