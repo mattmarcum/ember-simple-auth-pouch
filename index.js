88029b0832fcb0fb51fd136354a41fb801b4b05e
Joren l'Ami <joren@distinct.nl>
2016-11-30 14:31:03 +0100

    ember 2.7

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ var stew = require('broccoli-stew');
 module.exports = {
   name: 'ember-simple-auth-pouch',
 
-  init: function(parent, project) {
-    this._super(parent, project);
+  init: function() {
+  	this._super.init && this._super.init.apply(this, arguments);
 
     var bowerDeps = this.project.bowerDependencies();
 
-    if (bowerDeps['pouchdb.authentication']) {this.ui.writeWarnLine('Please remove `pouchdb.authentication` from `bower.json`. For ember-simple-auth-pouch only the NPM package is needed.');}
+    if (bowerDeps['pouchdb-authentication']) {this.ui.writeWarnLine('Please remove `pouchdb-authentication` from `bower.json`. For ember-simple-auth-pouch only the NPM package is needed.');}
   },
 
-  treeForVendor: function() {
-    return stew.find(path.join(path.dirname(require.resolve('pouchdb.authentication')), '..', 'dist'), {
+  treeForVendor: function() {  	
+    return stew.find(path.join(path.dirname(require.resolve('pouchdb-authentication')), '..', 'dist'), {
       destDir: 'pouchdb',
       files: ['pouchdb.authentication.js']
     });
@@ -24,5 +24,9 @@ module.exports = {
 
   included(app) {
     app.import('vendor/pouchdb/pouchdb.authentication.js');
-  }
+  },
+  
+  isDevelopingAddon: function() {
+  	return true;
+  },
 };
