92cfad97986d1d4d333651d5926dc1893d868ece
Martin Broerse <broerse@martinic.com>
2018-05-10 20:28:17 +0200

    firsts step

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* jshint node: true, -W030 */
 'use strict';
 
 var path = require('path');
@@ -15,29 +14,29 @@ module.exports = {
       throw new Error("'" + module + "' is not found, but required as a peerDependency. Please install it with `npm install --save-dev " + module + "`");
     }
   },
-  
+
   init: function() {
   	this._super.init && this._super.init.apply(this, arguments);
 
     var bowerDeps = this.project.bowerDependencies();
 
     if (bowerDeps['pouchdb-authentication']) {this.ui.writeWarnLine('Please remove `pouchdb-authentication` from `bower.json`. For ember-simple-auth-pouch only the NPM package is needed.');}
-    
+
     this.testPeerDependency('ember-pouch');
     this.testPeerDependency('ember-simple-auth');
   },
 
   treeForVendor: function() {
-    return stew.find(path.join(path.dirname(require.resolve('pouchdb-authentication')), '..', 'dist'), {
-      destDir: 'pouchdb',
-      files: ['pouchdb.authentication.js']
+    return stew.find(path.join(path.dirname(require.resolve('pouchdb-authentication')), '..', 'lib'), {
+      destDir: 'pouchdb-authentication',
+      files: ['index.browser.js']
     });
   },
 
   included(app) {
-    app.import('vendor/pouchdb/pouchdb.authentication.js');
+    app.import('vendor/pouchdb-authentication/index.browser.js');
   },
-  
+
   isDevelopingAddon() {
     return true;
   },
