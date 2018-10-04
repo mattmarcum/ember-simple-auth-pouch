07d436af6c76f0728dc79015f370616c0b1042a8
Martin Broerse <broerse@martinic.com>
2018-10-04 11:01:34 +0200

    First try to release v0.1.0

--- addon/authenticators/pouch.js
+++ addon/authenticators/pouch.js
@@ -1,54 +1,57 @@
 import Base from 'ember-simple-auth/authenticators/base';
-import Ember from 'ember';
-const { getOwner } = Ember;
+import { inject as service } from '@ember/service';
+import { getOwner } from '@ember/application';
+import { assert } from '@ember/debug';
+import { isEmpty } from '@ember/utils';
+import { reject } from 'rsvp';
 
 export default Base.extend({
-  store: Ember.inject.service(),
-  
+  store: service(),
+
   getDb() {
     if (this.db) return this.db;
-    
-  	let config = getOwner(this).resolveRegistration('config:environment');
+
+    let config = getOwner(this).resolveRegistration('config:environment');
 
     //let the user override the default adapter
     let pouchAdapterName = config.authAdapter || 'application';
 
     let pouchAdapter = this.get('store').adapterFor(pouchAdapterName);
 
-    Ember.assert('You must have an ember-pouch adapter setup for authentication', pouchAdapter);
-    
-  	this.db = pouchAdapter.db;
-  	
-  	return this.db;
+    assert('You must have an ember-pouch adapter setup for authentication', pouchAdapter);
+
+    this.db = pouchAdapter.db;
+
+    return this.db;
   },
 
   restore(data) {
-  	return this.getDb().getSession().then((resp) => {
-  		let result = null;
-  		if (!Ember.isEmpty(data.name) && resp.userCtx.name === data.name) {
-  			result = resp.userCtx;
-  			this.getDb().emit('loggedin');
-  		}
-  		else {
-  			result = Ember.RSVP.reject("Not logged in or incorrect user in cookie");
-  		}
-  		
-  		return result;
-  	});
+    return this.getDb().getSession().then((resp) => {
+      let result = null;
+      if (!isEmpty(data.name) && resp.userCtx.name === data.name) {
+        result = resp.userCtx;
+        this.getDb().emit('loggedin');
+      }
+      else {
+        result = reject("Not logged in or incorrect user in cookie");
+      }
+
+      return result;
+    });
   },
 
   authenticate(username, password) {
-  	return this.getDb().login(username, password)
-  	  .then(() => this.getDb().getSession())
-  	  .then((resp) => {
-    		this.getDb().emit('loggedin');
-    		return resp.userCtx;
-    	});
+    return this.getDb().login(username, password)
+      .then(() => this.getDb().getSession())
+      .then((resp) => {
+        this.getDb().emit('loggedin');
+        return resp.userCtx;
+      });
   },
 
   invalidate() {
     let result = this.getDb().logout();
-  	this.getDb().emit('loggedout');
+    this.getDb().emit('loggedout');
     return result;
   }
 });
